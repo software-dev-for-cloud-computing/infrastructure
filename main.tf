82f25e54329e47b5215ac3bffaed34a5d2e9c7f1
Tim Burkei <tb169@hdm-stuttgart.de>
2024-06-09 16:57:44 +0200

    switch to other sub

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "azurerm_service_plan" "app_service_plan" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   os_type             = "Linux"
-  sku_name            = "F1"
+  sku_name            = "B1"
 }
 
 resource "azurerm_linux_web_app" "react_app" {
@@ -28,7 +28,7 @@ resource "azurerm_linux_web_app" "react_app" {
 
   site_config {
     app_command_line = ""
-    always_on        = false
+    always_on        = true
 
     application_stack {
         docker_image_name = "docker/getting-started:latest"
