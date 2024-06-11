ccdd6b072918d095ca4c506a68d51854143370c1
Tim Burkei <tb169@hdm-stuttgart.de>
2024-06-11 10:25:14 +0200

    update mongodb collection and resource name

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "random_pet" "name" {
 }
 
 resource "azurerm_resource_group" "main" {
-  name     = "myResourceGroup202412"
+  name     = "myResourceGroup202413"
   location = "Germany West Central"
 }
 
