6da6f7a091cd271bb6855c7ca52d416e1dc34887
Tim Burkei <tb169@hdm-stuttgart.de>
2024-06-22 23:35:44 +0200

    update 4

--- main.tf
+++ main.tf
@@ -105,24 +105,3 @@ resource "azurerm_cosmosdb_mongo_collection" "mongo_collection" {
     unique = true
   }
 }
-
-output "cosmosdb_account_endpoint" {
-  value = azurerm_cosmosdb_account.cosmos_account.endpoint
-}
-
-output "cosmosdb_primary_key" {
-  value     = azurerm_cosmosdb_account.cosmos_account.primary_key
-  sensitive = true
-}
-
-output "cosmosdb_username" {
-  value = var.prod_db_user
-}
-
-output "cosmosdb_password" {
-  value = var.prod_db_password
-}
-
-output "cosmosdb_database_name" {
-  value = azurerm_cosmosdb_mongo_database.mongo_database.name
-}
