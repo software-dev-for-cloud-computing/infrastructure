231388a895c1804b8763a79a4ad99f957ad1d110
Tim Burkei <tb169@hdm-stuttgart.de>
2024-06-22 23:32:43 +0200

    update 3

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,20 @@
-
-
-/* Uncomment these outputs if the corresponding resources are declared and used
-
-output "react_app_url" {
-  value = azurerm_app_service.react_app.default_site_hostname
-} 
-
-output "fastapi_app_url" {
-  value = azurerm_app_service.fastapi_app.default_site_hostname
+output "cosmosdb_account_endpoint" {
+  value = azurerm_cosmosdb_account.cosmos_account.endpoint
 }
 
-output "nodejs_app_url" {
-  value = azurerm_app_service.nodejs_app.default_site_hostname
+output "cosmosdb_primary_key" {
+  value     = azurerm_cosmosdb_account.cosmos_account.primary_key
+  sensitive = true
 }
 
-output "storage_account_name" {
-  value = azurerm_storage_account.blob_storage.name
+output "cosmosdb_username" {
+  value = var.prod_db_user
 }
-*/
+
+output "cosmosdb_password" {
+  value = var.prod_db_password
+}
+
+output "cosmosdb_database_name" {
+  value = azurerm_cosmosdb_mongo_database.mongo_database.name
+}
