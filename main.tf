b4cdf82f3ac10b9b49a1d20b54579aedf45e65bb
Tim Burkei <tb169@hdm-stuttgart.de>
2024-08-16 10:27:43 +0200

    update env variablen

--- main.tf
+++ main.tf
@@ -140,9 +140,9 @@ resource "azurerm_container_group" "main_container" {
       MONGODB_URI = azurerm_cosmosdb_account.cosmos_account.connection_strings[0]
       NODE_ENV = "production"
       PORT = "3000"
-      CORS_ORIGIN = "http://react:80" 
-      AI_SERVICE_URL = "http://fastapi:8000/api/v1/qa"  
-      DOCUMENT_API_URL = "http://fastapi:8000/api/v1/document"  
+      CORS_ORIGIN = "http://${azurerm_container_group.main_container.ip_address}:80"
+      AI_SERVICE_URL = "http://fastapi:8000/api/v1/qa"
+      DOCUMENT_API_URL = "http://fastapi:8000/api/v1/document"
     }
   }
 
@@ -158,7 +158,7 @@ resource "azurerm_container_group" "main_container" {
     }
 
     environment_variables = {
-      REACT_APP_API_URL = "http://nodejs:3000"  
+      REACT_APP_API_URL = "http://${azurerm_container_group.main_container.ip_address}:3000"
     }
   }
 
