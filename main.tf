0c8d9d7fb73f9955c12088342970d765472f7e37
Tim Burkei <tb169@hdm-stuttgart.de>
2024-08-19 14:05:43 +0200

    test free plan for cosmos

--- main.tf
+++ main.tf
@@ -35,6 +35,8 @@ resource "azurerm_cosmosdb_account" "cosmos_account" {
   resource_group_name = azurerm_resource_group.main.name
   offer_type          = "Standard"
   kind                = "MongoDB"
+  free_tier_enabled = true
+
   consistency_policy {
     consistency_level = "Session"
   }
