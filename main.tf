b3a87fe18235656d72cd2849f9d980ac8f7afe26
Tim Burkei <tb169@hdm-stuttgart.de>
2024-08-21 12:46:33 +0200

    add virtual networks 2

--- main.tf
+++ main.tf
@@ -198,6 +198,15 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.v_net.name
   address_prefixes     = ["10.0.1.0/24"]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      # actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
 }
 
 resource "azurerm_network_interface" "network_interface" {
